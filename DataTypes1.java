1fc2b2912f496b2352ff038ef9642c0932ac5988
Divyawankhadejp <134001202+Divyawankhadejp@users.noreply.github.com>
2023-07-06 00:33:31 +0530

    Add files via upload

---
+++ DataTypes1.java
@@ -0,0 +1,21 @@
+public class DataTypes1
+{
+	public static void main(String args[])
+	{
+		
+		char a ='Z';
+		int i = 89;
+		byte b = 4;
+		short s = 56;
+		double d = 4.355453532;
+		float f = 4.7333423f;
+		
+		System.out.println("char:"+a);
+		System.out.println("integer:"+i);
+		System.out.println("byte:"+b);
+		System.out.println("short:"+s);
+		System.out.println("float:"+f);
+		System.out.println("double:"+d);
+	}
+}
+		
