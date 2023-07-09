01c1b50d35cc68121bea3f7f0a87d03cd752ccb1
Divyawankhadejp <134001202+Divyawankhadejp@users.noreply.github.com>
2023-07-09 19:32:04 +0530

    Add files via upload

---
+++ objects.java
@@ -0,0 +1,21 @@
+class Student
+{
+  int rollno = 20;
+  String name = "Ronak";
+  
+  Void display()
+  {
+  System.out.println("Roll NO:" = +rollno);
+  System.out.println("Name:" +name);
+  }
+ }
+ public class objects
+ {
+  public static Void main(String args[]);
+ {
+     Student s1;
+     s1 = new Student();
+     s1.display();
+ }
+}
+ 
