7c72e697bfc71c20a67654bec131763eea786e71
Divyawankhadejp <134001202+Divyawankhadejp@users.noreply.github.com>
2023-07-10 00:00:31 +0530

    Add files via upload

---
+++ Default.java
@@ -0,0 +1,23 @@
+class Student
+{
+   int rollno = 20;
+   String name = "Ronak";
+   
+   Student()
+   {
+	   System.out.println("Rollno:"+rollno);
+	   System.out.println("Name:"+name);
+   }
+}
+public class Default
+{
+	public static void main(String args[])
+	{
+		Student s1 = new Student();
+	}
+}
+	   
+	   
+   
+   
+  
